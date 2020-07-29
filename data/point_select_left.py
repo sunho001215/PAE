f67df10269a704451da70695cf37208c3ed07917
Ayoung Lee <aylee2008@naver.com>
2020-07-29 15:33:03 +0900

    point selection code modified

--- data/point_select_left.py
+++ data/point_select_left.py
@@ -112,7 +112,7 @@ while i<len(name):
             dy = dy/rt
             hei = (math.sqrt((pt1[0]-pt2[0])**2+(pt1[1]-pt2[1])**2)+math.sqrt((pt3[0]-pt4[0])**2+(pt3[1]-pt4[1])**2))/2
             wid = (math.sqrt((pt1[0]-pt4[0])**2+(pt1[1]-pt4[1])**2)+math.sqrt((pt3[0]-pt2[0])**2+(pt3[1]-pt2[1])**2))/2
-            dat.append((cen_x/800, cen_y/800, dx/800, dy/800, wid/800, hei/800))
+            dat.append((cen_x/800, cen_y/800, dx, dy, wid/800, hei/800))
             print(dat)
             cs = dx
             sn = dy
